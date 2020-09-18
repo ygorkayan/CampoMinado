c602c5d3a0c1b69748ba63766743704165f8f3f3
Ygor Kayan <ygorkayan@gmail.com>
2020-09-18 15:45:11 -0300

    inicio

---
+++ src/Index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, SafeAreaView } from "react-native";
+
+export default (props) => {
+  return (
+    <SafeAreaView>
+      <Text>ola mundo</Text>
+    </SafeAreaView>
+  );
+};
